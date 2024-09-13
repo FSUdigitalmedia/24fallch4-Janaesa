fdccb42d664e4027fd9810cabbd817c3abe06b20
Janaesa Smith <jcs22n@fsu.edu>
2024-09-13 10:47:12 -0400

    stopped the circle from moving ( success)

--- sketch.js
+++ sketch.js
@@ -7,8 +7,17 @@ function setup() {
 
 function draw() {
   background(125);
+
+  //Size of circle changes with the horizontal mouse position
   let d = map(mouseX, 0, width, 50, 150, true);
-  circle(width/2, y, d);
-  
+  let yPos = map2(y, 0, height, 0, height, QUADRATIC, BOTH)
+  circle(width/2, yPos, yPos);
+
+  if (y > height || y < 0) {
+    speed = 0
+  }
+  y = y + speed;
+
+
 
 }
